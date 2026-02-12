e9606aee08c22b9d18715e1255210c612ad6b701
Jakkarin004 <jakrin977@gmail.com>
2026-02-12 09:42:52 +0700

    Feature/new repo (#149)
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Merge branch 'dev' into feature/newRepo
    
    # Conflicts:
    #	src/app/app.component.ts
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * Revert "no message"
    
    This reverts commit d4e43bcd7bad1654ef217bb8ceaf4e2333c6a19b.
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message

--- src/app/interface/issues_interface.ts
+++ src/app/interface/issues_interface.ts
@@ -2,40 +2,40 @@ import { commentResponseDTO } from "./comment_interface"
 import { UserInfo } from "./user_interface"
 
 export interface IssuesResponseDTO {
-    id: string,
-    scanId: string,
-    projectData: {
+  id: string,
+  scanId: string,
+  projectId?: string,
+  projectData?: {
     id: string,
     name: string
-    },
-    issueKey: string,
-    type: string,
-    severity: string,
-    ruleKey: string,
-    component: string,
-    line: number,
-    message: string,
-    assignedTo: {
+  },
+  issueKey: string,
+  type: string,
+  severity: string,
+  ruleKey: string,
+  component: string,
+  line: number,
+  message: string,
+  assignedTo?: {
     id: string,
     username: string,
     email: string,
     role: string,
     phone: string,
     createAt: string
-    },
-    status: string,
-    createdAt: string,
-    commentData:  commentResponseDTO[]
-  }
+  },
+  status: string,
+  createdAt: string,
+  commentData: commentResponseDTO[]
+}
 
-  export interface IssuesRequestDTO {
-    id: string,
-    status: string,
-    assignedTo: string
-  }
-    export interface IssuesDetailResponseDTO {
-    description: string,
-    vulnerableCode: string,
-    recommendedFix: string  
-  }
-  
+export interface IssuesRequestDTO {
+  id: string,
+  status: string,
+  assignedTo: string
+}
+export interface IssuesDetailResponseDTO {
+  description: string,
+  vulnerableCode: string,
+  recommendedFix: string
+}
